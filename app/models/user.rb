b9e23916bbac8ab914b3ef67b9f486ce36bac2eb
DanCSF <m210amg@gmail.com>
2018-10-07 13:23:16 +0300

    refactor views and controllers jbuilder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
-  acts_as_token_authenticatable
+  has_many :contacts
 end
